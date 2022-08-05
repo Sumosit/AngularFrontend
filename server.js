488623c296e955ce4e5c02f95dc3a49fe05e9fcf
Yerlan <isaev.yerlan@kase.kz>
2022-08-05 11:56:52 +0300

    netlify settings

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/<app-name>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
